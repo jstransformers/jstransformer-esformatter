febbf243c1287efcc8c4049e9a710651496ece2d
Caleb Eby <caleb.eby01@gmail.com>
2016-12-17 21:13:34 -0800

    Fix xo errors in index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict'
 
-var esformatter = require('esformatter');
+var esformatter = require('esformatter')
 
-exports.name = 'esformatter';
-exports.outputFormat = 'jsx';
+exports.name = 'esformatter'
+exports.outputFormat = 'jsx'
 
-exports.render = esformatter.format;
+exports.render = esformatter.format
